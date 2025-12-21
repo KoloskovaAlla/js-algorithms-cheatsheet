6df97daf2d333ecfc2e88525dc6ce7cb1056c8d0
koloskova-ay <koloskova-ay@m13.ru>
2025-12-21 21:28:43 +0300

    getLetterFromIndex

---
+++ strings/index-to-letter.js
@@ -0,0 +1,8 @@
+const getLetterFromIndex = (index) => {
+    return String.fromCharCode(index + 65)
+};
+
+console.log(getLetterFromIndex(0))
+console.log(getLetterFromIndex(1))
+console.log(getLetterFromIndex(2))
+
